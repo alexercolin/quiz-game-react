8dc37ea4a921b0fa7d55df00492aa2c983ac3bb4
alexercolin <alex_ercolin@hotmail.com>
2021-10-08 14:36:48 -0300

    feature: add modal

--- src/components/InputQuestion/index.js
+++ src/components/InputQuestion/index.js
@@ -1,30 +1,60 @@
 import { useState } from "react";
-import { TextField, Button } from "@material-ui/core";
+import {
+  TextField,
+  Button,
+  Modal,
+  Typography,
+  Box,
+  FormControl,
+} from "@material-ui/core";
 import { api } from "../service/api";
+import "./style.css";
 
-const InputQuestion = () => {
+const InputQuestion = (props) => {
   const [question, setQuestionsNumber] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   function handleSubmit() {
-    api.get(`?amount=${question}`).then((response) => {
-      console.log(response.data.results);
-    });
+    if (question > 1) {
+      return api.get(`?amount=${question}`).then((response) => {
+        props.onShowQuestions(response.data.results);
+      });
+    }
+    window.alert('Favor insira um número maior que zero no campo de perguntas')
   }
+
   return (
-    <div>
+    <FormControl>
       <TextField
         id="outlined-number"
-        label="Number"
+        label="Número de perguntas"
         type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
         onChange={(event) => setQuestionsNumber(event.target.value)}
       />
       <Button onClick={handleSubmit} variant="contained">
-        Confirmar
+        Start Game!
       </Button>
-    </div>
+      <Button onClick={handleOpenModal} variant="outlined">
+        Cancel
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="warning-modal">
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Hey friend!
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            You need to chose a number to start the game =D
+          </Typography>
+        </Box>
+      </Modal>
+    </FormControl>
   );
 };
 
