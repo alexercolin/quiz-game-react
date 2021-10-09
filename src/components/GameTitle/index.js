3e233e614d5fc30873fc18e167203a60d040c223
alexercolin <alex_ercolin@hotmail.com>
2021-10-09 08:25:45 -0300

    fix:identation and remove modal

--- src/components/GameTitle/index.js
+++ src/components/GameTitle/index.js
@@ -1,15 +1,15 @@
 import { Typography } from "@material-ui/core";
-import './style.css'
-
+import "./style.css";
 
 const GameTitle = () => {
-    return(
-        <div className="quiz-title">
-        <Typography variant="h4">Welcome to the quiz game!</Typography>
-        <Typography variant="h5">Choose a number of questions and test your knowledge!</Typography>
-        </div>
-    )
+  return (
+    <div className="quiz-title">
+      <Typography variant="h4">Welcome to the quiz game!</Typography>
+      <Typography variant="h5">
+        Choose a number of questions and test your knowledge!
+      </Typography>
+    </div>
+  );
+};
 
-}
-
-export default GameTitle
+export default GameTitle;
--- src/components/InputQuestion/index.js
+++ src/components/InputQuestion/index.js
@@ -1,20 +1,10 @@
 import { useState } from "react";
-import {
-  TextField,
-  Button,
-  Modal,
-  Typography,
-  Box,
-  FormControl,
-} from "@material-ui/core";
+import { TextField, Button, FormControl } from "@material-ui/core";
 import { api } from "../service/api";
 import "./style.css";
 
 const InputQuestion = (props) => {
   const [question, setQuestionsNumber] = useState("");
-  const [open, setOpen] = useState(false);
-  const handleOpenModal = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   function handleSubmit() {
     if (question > 1) {
@@ -22,42 +12,25 @@ const InputQuestion = (props) => {
         props.onShowQuestions(response.data.results);
       });
     }
-    window.alert("Favor insira um número maior que zero no campo de perguntas");
+    window.alert(
+      "Please enter a number greater than zero in the question field"
+    );
   }
 
   return (
     <FormControl>
       <div className="question-section">
         <div className="question-input">
-        <TextField
-          id="outlined-number"
-          label="Número de perguntas"
-          type="number"
-          onChange={(event) => setQuestionsNumber(event.target.value)}
-        />
+          <TextField
+            id="outlined-number"
+            label="Number of questions"
+            type="number"
+            onChange={(event) => setQuestionsNumber(event.target.value)}
+          />
         </div>
         <Button size="large" onClick={handleSubmit} variant="contained">
           Start Game!
         </Button>
-        <Button size="small" onClick={handleOpenModal} variant="outlined">
-          Cancel
-        </Button>
-
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box className="warning-modal">
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Hey friend!
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              You need to chose a number to start the game =D
-            </Typography>
-          </Box>
-        </Modal>
       </div>
     </FormControl>
   );
